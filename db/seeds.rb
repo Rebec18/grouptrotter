8761805042c72b3cddf5928cb3501d35e0bd53a4
Rebecca Giaccone <rebecbibas@gmail.com>
2020-11-30 18:27:56 +0100

    creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+Traveller.destroy_all
+Group.destroy_all
+
+depart = DateTime.new(2020, 12, 12).strftime("%d/%m/%Y")
+arrivee = DateTime.new(2020, 12, 21).strftime("%d/%m/%Y")
+depart1 = DateTime.new(2021, 2, 8).strftime("%d/%m/%Y")
+arrivee1 = DateTime.new(2021, 2, 14).strftime("%d/%m/%Y")
+
+group_friends = Group.create(date_from: depart, date_to: arrivee, fly_to: "Mykonos")
+group_family = Group.create(date_from: depart1, date_to: arrivee1)
+
+claire = Traveller.create(name:"Claire", fly_from: "Rennes", price_from: 1, price_to: 3000, group: group_friends)
+farouk = Traveller.create(name:"Farouk", fly_from: "Nice", price_from: 1, price_to: 500, group: group_friends)
+mateo = Traveller.create(name:"Matéo", fly_from: "Marseille", price_from: 1, price_to: 200, group: group_friends)
+rebec = Traveller.create(name:"Rebecca", fly_from: "Marseille", price_from: 1, price_to: 1500, group: group_friends)
+
+marie_jo = Traveller.create(name:"Marie-Jo", fly_from: "Lyon", price_from: 1, price_to: 600, group: group_family)
+michel = Traveller.create(name:"Michel", fly_from: "Paris", price_from: 1, price_to: 300, group: group_family)
