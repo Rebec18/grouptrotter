3c7378e08cb16decb24bb9c14f826464a7c12552
Farouk Bounou <farouk.bounou@gmail.com>
2020-12-01 13:17:36 +0100

    crud group et traveler

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :groups, only: [:show, :create]
+  resources :groups, only: [:show, :create, :new] do
+    resources :travelers, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
