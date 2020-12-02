547ff2af89a21dee1a0b0c4357ff8dba1aa8740d
Claire Guillou <guillou.clr@gmail.com>
2020-12-02 20:18:40 +0100

    multi result avec les bons AR

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,18 +21,27 @@ class GroupsController < ApplicationController
   end
 
 #méthode de recherche de vol
+
   def search
     @group = Group.find(params[:id])
-
-    @search = []
+    #création d'un tableau avec l'ensemble des données des voyageurs
+    @search_a = []
+    @search_r = []
     #itère sur les différents travelers pour passer en multi recherches
     @group.travelers.each do |traveler|
        #parse des infos de vols
-      reponse = RestClient.get "https://api.skypicker.com/flights?fly_from=#{traveler.fly_from}&fly_to=#{@group.fly_to}&date_from=#{@group.date_from.strftime("%d/%m/%Y")}&date_to=#{@group.date_from.strftime("%d/%m/%Y")}&flight_type=return&return_from=#{@group.date_to.strftime("%d/%m/%Y")}&return_to=#{@group.date_to.strftime("%d/%m/%Y")}&price_from=1&price_to=#{traveler.price_to}&direct_flights=1&partner=grouptrottergrouptrotter&v=3&curr=EUR"
-      @search << JSON.parse(reponse)["data"]
+        aller = RestClient.get "https://api.skypicker.com/flights?fly_from=#{traveler.fly_from}&fly_to=#{@group.fly_to}&date_from=#{traveler.date_from.strftime("%d/%m/%Y")}&date_to=#{traveler.date_from.strftime("%d/%m/%Y")}&price_from=1&price_to=#{traveler.price_to}&direct_flights=1&partner=grouptrottergrouptrotter&v=3&curr=EUR"
+        @search_a << JSON.parse(aller)["data"]
+
+      @search_a.first.each do |hash|
+        retour = RestClient.get "https://api.skypicker.com/flights?fly_from=#{hash["flyTo"]}&fly_to=#{traveler.fly_from}&date_from=#{traveler.date_to.strftime("%d/%m/%Y")}&date_to=#{traveler.date_to.strftime("%d/%m/%Y")}&price_from=1&price_to=#{traveler.price_to}&direct_flights=1&partner=grouptrottergrouptrotter&v=3&curr=EUR"
+        bertrand = JSON.parse(retour)["data"]
+        @search_r << bertrand unless bertrand == []
+      end
     end
 
-    #LA BONNE URL https://api.skypicker.com/flights?fly_from=#{@group.travelers.first.fly_from}&fly_to=#{@group.fly_to}&date_from=#{@group.date_from.strftime("%d/%m/%Y")}&date_to=#{@group.date_from.strftime("%d/%m/%Y")}&flight_type=return&return_from=#{@group.date_to.strftime("%d/%m/%Y")}&return_to=#{@group.date_to.strftime("%d/%m/%Y")}&price_from=1&price_to=#{@group.travelers.first.price_to}&direct_flights=1&partner=grouptrottergrouptrotter&v=3&curr=EUR
+    #test avec la bonne url "https://tequila-api.kiwi.com/v2/search?apikey=dA_ZyNbfWwC6tB6h1iwevDVUybsLVp4U&fly_from=MRS&fly_to=europe&date_from=12/12/2020&date_to=12/12/2020&flight_type=round&return_from=14/12/2020&return_to=14/12/2020&price_from=1&price_to=300&direct_flights=1&partner=grouptrottergrouptrotter&v=3&curr=EUR"
+    #LA BONNE URL "https://api.skypicker.com/flights?fly_from=#{traveler.fly_from}&fly_to=#{@group.fly_to}&date_from=#{traveler.date_from.strftime("%d/%m/%Y")}&date_to=#{traveler.date_from.strftime("%d/%m/%Y")}&flight_type=return&return_from=#{traveler.date_to.strftime("%d/%m/%Y")}&return_to=#{traveler.date_to.strftime("%d/%m/%Y")}&price_from=1&price_to=#{traveler.price_to}&direct_flights=1&partner=grouptrottergrouptrotter&v=3&curr=EUR"
     # test = RestClient.get "https://api.skypicker.com/flights?fly_from=MRS&fly_to=ORY&date_from=12/12/2020&date_to=12/12/2020&flight_type=return&return_from=14/12/2020&return_to=14/12/2020&price_from=1&price_to=300&direct_flights=1&partner=grouptrottergrouptrotter&v=3&curr=EUR"
   end
 
