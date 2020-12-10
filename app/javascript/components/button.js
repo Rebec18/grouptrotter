8970a727d0c1396e3ab844d917fed9521c845d68
Rebecca Giaccone <rebecbibas@gmail.com>
2020-12-10 16:36:37 +0100

    boutton plus marche pas

---
+++ app/javascript/components/button.js
@@ -0,0 +1,10 @@
+
+// let plusbut = []
+// plusbut = document.querySelectorAll('div.see-more');
+// for(let i = 0; i < plusbut.count; i++) {
+//   let autreticket = plusbut[i].previousElementSibling
+//   plusbut[i].addEventListener("click", () => {
+//     let number = <%= @fat_hash[@group.travelers[i]].count %>
+//     autreticket.style.height = `${188*number}px`;
+//   });
+// }
